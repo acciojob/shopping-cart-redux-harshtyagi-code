c9a59ae266054399fd8b47be110af157c85c22c4
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-04-01 12:17:56 +0000

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
 import React from "react";
+import Footer from "./Footer"
+import Home from "./Home"
+import Navbar from "./Navbar"
 import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+        <Navbar/>
+        <Home/>
+        <Footer/>
     </div>
   )
 }
